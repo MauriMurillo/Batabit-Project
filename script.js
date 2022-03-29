a984c1b2a8b58285e00fdde1bc256f8455990b8d
Mauricio Murillo Gonzales <mauriciomurillogonzales@Mauricios-MacBook-Pro.local>
2022-03-29 12:26:29 -0400

    finished mobile site
    added options slide show, fixed some styles and position

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let slide_exchange_position = 0;
-let slide_options_position = 0;
+let slide_options_position = 1;
 
 
 function slide(){
@@ -19,4 +19,57 @@ function slide(){
         slides[0].style.gridArea = "two";
     }
     return;
+}
+let arrowleftoptions = document.getElementById("arrow-left-options");
+let arrowrightoptions = document.getElementById("arrow-right-options");
+let slidesoptions = document.getElementsByClassName("options-grid-pane");
+
+
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+let life = document.getElementById('life');
+
+
+function slide_options_left(){
+    slide_options_position -= 1;
+    if (slide_options_position == 0) {
+        arrowleftoptions.style.visibility = "hidden";
+        arrowrightoptions.style.visibility = "visible";
+        slidesoptions[0].style.gridArea = "two";
+        slidesoptions[1].style.gridArea = "one";
+        slidesoptions[2].style.gridArea = "three";
+        month.style.visibility = "visible"
+        year.style.visibility = "hidden"
+    }
+
+    if (slide_options_position == 1) {
+        arrowleftoptions.style.visibility = "visible";
+        arrowrightoptions.style.visibility = "visible";
+        slidesoptions[0].style.gridArea = "one";
+        slidesoptions[1].style.gridArea = "two";
+        slidesoptions[2].style.gridArea = "three";
+        year.style.visibility = "visible"
+        life.style.visibility = "hidden"
+    }
+}
+function slide_options_right(){
+    slide_options_position += 1;
+    if (slide_options_position == 2) {
+        arrowrightoptions.style.visibility = "hidden";
+        arrowleftoptions.style.visibility = "visible";
+        slidesoptions[0].style.gridArea = "one";
+        slidesoptions[1].style.gridArea = "three";
+        slidesoptions[2].style.gridArea = "two";
+        life.style.visibility = "visible"
+        year.style.visibility = "hidden"
+    }
+    if (slide_options_position == 1) {
+        arrowrightoptions.style.visibility = "visible";
+        arrowleftoptions.style.visibility = "visible";
+        slidesoptions[0].style.gridArea = "one";
+        slidesoptions[1].style.gridArea = "two";
+        slidesoptions[2].style.gridArea = "three";
+        year.style.visibility = "visible"
+        month.style.visibility = "hidden"
+    }
 }
